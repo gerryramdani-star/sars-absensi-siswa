67bb3f628531df4bdc14cc4bf14c94770d891bf7
gerryramdani-star <gerry.ramdani@gmail.com>
2025-08-28 17:08:37 +0700

    Fix: Hapus firebase-config.js dari pelacakan

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// File ini HANYA untuk pengembangan di komputer lokal.
-const firebaseConfig = {
-  apiKey: "AIzaSyCR3m1SsXUeFusEYmLlWww9g3JX4zsOs5s",
-  authDomain: "rekap-absensi-siswa-e4ba8.firebaseapp.com",
-  projectId: "rekap-absensi-siswa-e4ba8",
-  storageBucket: "rekap-absensi-siswa-e4ba8.appspot.com",
-  messagingSenderId: "176492172190",
-  appId: "1:176492172190:web:529c936d86e91d924dbc8f"
-};
